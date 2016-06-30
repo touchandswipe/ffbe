60534c556b32750e4e7f999d8d0604c9fba78bc6
Sam GreenP <greenpuck55@gmail.com>
2016-07-01 00:50:59 +1000

    Update commoncode.js

--- js/commoncode.js
+++ js/commoncode.js
@@ -1,1 +1,10 @@
-
+/*JQ Param handling*/
+function urlParam(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return '';
+    }
+    else{
+       return decodeURIComponent(results[1]) || '';
+    }
+}
